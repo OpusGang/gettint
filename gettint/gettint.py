0ff1812bfdf0a79d2ca609b045bb568735d12bb8
noizuy <85082087+noizuy@users.noreply.github.com>
2022-06-26 22:04:30 +0200

    gettint force matchcolors and rgb gamma test

--- gettint/gettint.py
+++ gettint/gettint.py
@@ -47,7 +47,6 @@ def _gamma(c, gamma):
 def _gamma_bug(c):
     return _gamma(c, 0.88)
 
-
 @yuv_adj
 def _TVtoPC(c):
     return c.resize.Point(range_in=0, range=1)
@@ -90,6 +89,12 @@ def _offset_gain(c, gains, offsets):
     return c.std.Expr([f"x {gains[i]} * {offsets[i]} +" for i in range(c.format.num_planes)])
 
 
+@rgb_adj
+def _gamma_rgb(c, gammas):
+    max_val = (2 << c.format.bits_per_sample) - 1
+    return c.std.Expr([f"x {max_val} / {gammas[i][0]} pow {max_val} *" for i in range(c.format.num_planes)])
+
+
 @yuv_adj
 def _levels(c, levels):
     c = split(c)
@@ -102,13 +107,15 @@ def _levels(c, levels):
 
 def gettint(src: str,
             ref: str,
-            frame: int = None):
+            frame: int = None,
+            force_matchcolors: bool = False):
     """
     A script to automate finding the right detinting method.
 
     :param src: Tinted source.
     :param ref: Untinted reference.
     :param frame: Frame number.  Defaults to middle of clip.
+    :param force_matchcolors: Force matchcolors script by bypassing common checks.
     """
     clips = [core.lsmas.LWLibavSource(src), core.lsmas.LWLibavSource(ref)]
 
@@ -146,29 +153,33 @@ def gettint(src: str,
 
     backup_tests = {
             "gamma"       : lambda c: _gamma(c, match_res[0][0]),
-            "offset+gain" : lambda c: _offset_gain(c, [round(vals[0], 3) for vals in match_res[1]], [round(vals[1], 3)
-                for vals in match_res[1]]),
-            "levels"      : lambda c: _levels(c, match_res[2])
+            "gamma RGB"   : lambda c: _gamma_rgb(c, match_res[1]),
+            "offset+gain" : lambda c: _offset_gain(c, [round(vals[0], 3) for vals in match_res[2]], [round(vals[1], 3)
+                for vals in match_res[2]]),
+            "levels"      : lambda c: _levels(c, match_res[3])
             }
 
-    # try to find the tint in common_tests
-    results = {}
-    for test in common_tests.keys():
-        results[test] = _test_adj(common_tests[test][0], clips[0], clips[1])
-        print(f"{test} difference: {round(results[test], 3)}")
-
-    # find lowest diff and check if below predefined threshold
-    lowest = min(results.values())
-    lowest_key = [key for key in results if results[key] == lowest and results[key] < common_tests[key][1]]
-    if lowest_key:
-        print("Likeliest tint fix(es):", ", ".join(lowest_key))
-        return
-
-    print("No common tint identified.  Running backup matchcolors tests.")
+    if not force_matchcolors:
+        # try to find the tint in common_tests
+        results = {}
+        for test in common_tests.keys():
+            results[test] = _test_adj(common_tests[test][0], clips[0], clips[1])
+            print(f"{test} difference: {round(results[test], 3)}")
+    
+        # find lowest diff and check if below predefined threshold
+        lowest = min(results.values())
+        lowest_key = [key for key in results if results[key] == lowest and results[key] < common_tests[key][1]]
+        if lowest_key:
+            print("Likeliest tint fix(es):", ", ".join(lowest_key))
+            return
+    
+        print("No common tint identified.  Running backup matchcolors tests.")
+    else:
+        results = {}
 
     # fall back to running matchcolors and try backup_tests
-    match_tests = ["gamma", ["gain", "offset"], "levels"]
-    match_fmts = [vs.YUV444P8, vs.RGB24, vs.YUV444P8]
+    match_tests = ["gamma", "gamma", ["gain", "offset"], "levels"]
+    match_fmts = [vs.YUV444P8, vs.RGB24, vs.RGB24, vs.YUV444P8]
     match_res = []
     
     for i in range(len(match_tests)):
@@ -198,10 +209,11 @@ def main():
     parser.add_argument(dest="src", type=str, help="Path to source file")
     parser.add_argument(dest="ref", type=str, help="Path to reference file")
     parser.add_argument("-f", "--frame", dest="frame", type=int, help="Frame number", required=False, default=None)
+    parser.add_argument("-m", "--force-matchcolors", dest="force_matchcolors", help="Force matchcolors", action="store_true")
 
     args = parser.parse_args()
 
-    gettint(args.src, args.ref, args.frame)
+    gettint(args.src, args.ref, args.frame, args.force_matchcolors)
 
 if __name__ == "__main__":
     main()
